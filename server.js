baea7a5a51c44b888c211f29f5a86401788228c9
jessy <jessy.baer@smartpanda.com>
2018-07-12 21:58:09 +0200

    add lcd control

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 // app.engine('html', require('ejs').renderFile);
 
 app.get("/", (req, res) => {
@@ -9,6 +12,11 @@ app.get("/", (req, res) => {
 
 app.get("/startLight", (req, res) => {
     // Todo send order to the robot
+    res.sendStatus(200);
+})
+
+app.post("/lcd", (req, res) => {
+    console.log(req.body.text);
     res.sendStatus(200);
 })
 
